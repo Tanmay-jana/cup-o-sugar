ea24f0a0d6bd867a79d2e1aacd379f7061cacbd7
Tanmay Jana <tanmay.jana@mountblue.io>
2020-05-03 11:40:13 +0530

    added mobile screens

--- src/Components/LandingPage/Header/Header.js
+++ src/Components/LandingPage/Header/Header.js
@@ -1,51 +1,80 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from 'react'
-import './Header.css'
-import Logo from '../../../assets/Logo.png';
+import React, { Component } from "react";
+import "./Header.css";
+import Logo from "../../../assets/Logo.png";
+import LogoWhite from "../../../assets/LogoWhite.png";
 
 export default class Header extends Component {
-    state = {
-        scrolled: false
-      };
+  state = {
+    scrolled: false,
+    isNav: false
+  };
 
-      componentDidMount = () => {
-        window.addEventListener("scroll", () => {
-          const isTop = window.scrollY < 100;
-          if (isTop !== true) {
-            this.setState({ scrolled: true });
-          } else {
-            this.setState({ scrolled: false });
-          }
-        });
-      };
-    
-      componentWillUnmount() {
-        window.removeEventListener("scroll", () => {
-          const isTop = window.scrollY < 100;
-          if (isTop !== true) {
-            this.setState({ scrolled: true });
-          } else {
-            this.setState({ scrolled: false });
-          }
-        });
+  isNav = () => {
+    this.setState({isNav: !this.state.isNav})
+  }
+
+  onClose = () => {
+    this.setState({isNav: false})
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== true) {
+        this.setState({ scrolled: true });
+      } else {
+        this.setState({ scrolled: false });
       }
-    render() {
-        return (
-            <div className = {this.state.scrolled === false ? "header-container": "header-container1"}>
-                <img className = "logo" src = {Logo} alt = "logo"/>
-                <div className = "link-container">
-                    <a href = "#">Home</a>
-                    <a href = "#">About</a>
-                    <a href = "#">Contact</a>
-                </div>
-                <div className = {this.state.scrolled === false ?"social-container" : "social-container1"}>
-                <i className="fa fa-facebook"></i>
-                <i className="fa fa-linkedin"></i>
-                <i className="fa fa-twitter"></i>
-                <i className="fa fa-youtube-play"></i>
-                <i className="fa fa-instagram"></i>
-                </div>
-            </div>
-        )
-    }
+    });
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== true) {
+        this.setState({ scrolled: true });
+      } else {
+        this.setState({ scrolled: false });
+      }
+    });
+  }
+  render() {
+    return (
+      <div
+        className={
+          this.state.scrolled === false
+            ? "header-container"
+            : "header-container1"
+        }
+      >
+        <img className="logo" src={Logo} alt="logo" />
+        <img className="logo-white" src={LogoWhite} alt="logo" />
+        <i onClick = {this.isNav} className="fa fa-bars nav-icon"></i>
+        <div className={this.state.isNav === true ?"myLinks" : "invisible"}>
+          <a onClick = {this.onClose} href="#">Home</a>
+          <a onClick = {this.onClose} href="#">About</a>
+          <a onClick = {this.onClose} href="#">Contact</a>
+        </div>
+        <div className="link-container">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+        <div
+          className={
+            this.state.scrolled === false
+              ? "social-container"
+              : "social-container1"
+          }
+        >
+          <i className="fa fa-facebook"></i>
+          <i className="fa fa-linkedin"></i>
+          <i className="fa fa-twitter"></i>
+          <i className="fa fa-youtube-play"></i>
+          <i className="fa fa-instagram"></i>
+        </div>
+      </div>
+    );
+  }
 }
